436a93b01f5c48e84dc1587450de191c6f0ba5c4
VidaNueva <vidanuevafundacion830@gmail.com>
2026-06-15 02:15:36 -0500

    hola

--- solicitudes_vn/urls.py
+++ solicitudes_vn/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include, re_path # <--- Añade re_path aquí
+from django.urls import path, include
 from django.conf import settings
-from django.views.static import serve # <--- Añade serve aquí
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('solicitudes.urls')),
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?s)(.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+# Método definitivo y limpio para servir media en Railway
+if settings.MEDIA_URL and settings.MEDIA_ROOT:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
