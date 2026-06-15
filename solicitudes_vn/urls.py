a01160853287fc5be97a633a7c2cefc380697d8f
VidaNueva <vidanuevafundacion830@gmail.com>
2026-06-15 00:10:11 -0500

    a

--- solicitudes_vn/urls.py
+++ solicitudes_vn/urls.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from solicitudes import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('solicitudes.urls')),
 ]
 
-
+# Estos handler están perfectos, déjalos así
 handler404 = views.error_404
 handler500 = views.error_500
 handler403 = views.error_403
-urlpatterns += static(
-    settings.STATIC_URL,
-    document_root=settings.STATICFILES_DIRS[0]
-)
+
+# ELIMINA todo el bloque de static(...) que tenías abajo.
+# WhiteNoise ya lo gestiona automáticamente por ti.
